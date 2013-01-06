10b37ab661bf8834c541dd0803d0bd6eae802600
Ken Ledbetter <ken.ledbetter@gmail.com>
2013-01-06 12:09:49 -0600

    Removing unused files

--- test/unit/helpers/raw_names_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class RawNamesHelperTest < ActionView::TestCase
-end
--- test/unit/matched_name_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class MatchedNameTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
